e3853d53e71b5e32a2feab331e4356c7b220ae6a
Batuhan YAMAN <batuhan.yaman@hacettepe.edu.tr>
2016-12-25 15:48:30 +0200

    config file for database and other options

---
+++ config.php
@@ -0,0 +1,24 @@
+﻿<?php
+	class DatabaseConnect
+	{
+	private $db_host = "localhost";
+	private $db_user = "batuhan";
+	private $db_pass = "123321";
+	private $db_name = "bus";
+	private $db;
+			function __construct(){}
+		
+			public function Connect()
+			{
+			 $this->db =  new  mysqli($this->db_host,$this->db_user,$this->db_pass,$this->db_name); // We access the Database
+				
+				// checking connection
+				if (mysqli_connect_errno()) {
+					printf("Connect failed: %s\n", mysqli_connect_error());
+					exit();
+				}	
+				
+				return  $this->db;
+			}
+	}
+?>
